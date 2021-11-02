3892ea63d379ca5dce16db0316ef76b3b394a825
allanlimo02 <allan.limo@student.moringaschool.com>
2021-11-02 17:46:35 +0300

    Created log in page

--- app/src/main/java/com/moringaschool/covidinformer/UI/LogIn.java
+++ app/src/main/java/com/moringaschool/covidinformer/UI/LogIn.java
@@ -2,15 +2,46 @@ package com.moringaschool.covidinformer.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.moringaschool.covidinformer.R;
 
-public class LogIn extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LogIn extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.username) EditText mUsername;
+    @BindView(R.id.password) EditText mPassword;
+    @BindView(R.id.login) Button mLoginButton;
+    @BindView(R.id.signup) TextView mSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        ButterKnife.bind(this);
+        mLoginButton.setOnClickListener(this);
+        mSignup.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==mSignup){
+            Intent intent=new Intent(LogIn.this, SignUp.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+        if(view==mLoginButton){
+            String username=mUsername.getText().toString();
+            String password=mPassword.getText().toString();
+
+
+        }
     }
 }
