51ef3a045e258b1c9e9d6f8f2c1f2a10164b94c0
andreassenmarvin <marvinandreassen@gmail.com>
2021-11-03 12:25:45 +0300

    Add api key

---
+++ app/src/main/java/com/moringaschool/covidinformer/Constant.java
@@ -0,0 +1,6 @@
+package com.moringaschool.covidinformer;
+
+public class Constant {
+    public static final String RAPID_BASE_URL = "https://covid-193.p.rapidapi.com";
+    public final String x_rapidapi_key = BuildConfig.x_rapidapi_key;
+}
