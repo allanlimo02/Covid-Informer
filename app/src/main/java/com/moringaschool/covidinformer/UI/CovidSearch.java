d5215ee8057f3d18262a078fff631a523bbc67b3
allanlimo02 <allan.limo@student.moringaschool.com>
2021-11-04 13:59:22 +0300

    Added Sign out

--- app/src/main/java/com/moringaschool/covidinformer/UI/CovidSearch.java
+++ app/src/main/java/com/moringaschool/covidinformer/UI/CovidSearch.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.covidinformer.R;
 
 import butterknife.BindView;
@@ -24,6 +28,29 @@ public class CovidSearch extends AppCompatActivity implements View.OnClickListen
 
         ButterKnife.bind(this);
         mSearchButton.setOnClickListener(this);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logout) {
+            logout();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent=new Intent(CovidSearch.this,LogIn.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
     @Override
