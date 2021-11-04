6aa870f8e0d5c5e9e4eb210edc9648491ea70436
andreassenmarvin <marvinandreassen@gmail.com>
2021-11-04 19:07:47 +0300

    Create RecyclerAdapter

--- app/src/main/java/com/moringaschool/covidinformer/Connection/ApiConn.java
+++ app/src/main/java/com/moringaschool/covidinformer/Connection/ApiConn.java
@@ -1,7 +1,5 @@
 package com.moringaschool.covidinformer.Connection;
 
-import com.moringaschool.covidinformer.model.CovidSearchResponse;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
